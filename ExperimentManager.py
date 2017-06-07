514b727e24577792ee528c66dcbe810f232c246d
sangchankim <sck970112@gmail.com>
2017-06-07 15:14:26 -0400

    Working experiment manager

--- ExperimentManager.py
+++ ExperimentManager.py
@@ -1,11 +1,30 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 from kafka import KafkaConsumer
+from kafka import KafkaProducer
 import datetime
+import json
 
 
-sched = BlockingScheduler()
-consumer = KafkaConsumer('beaker_topic', bootstrap_servers='localhost:1234')
+producer = KafkaProducer(value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+KAFKA_TOPIC = 'beakertopic'
 
+def executeExperimentJob(a):
+
+
+
+
+sched = BackgroundScheduler()
+sched.start()
+print 'b'
+
+consumer = KafkaConsumer('beakertopic')
 while True:
 	for msg in consumer:
-		print msg
+		data = json.loads(msg.value)
+		if data['type'] == 'AddExperiment':
+			sched.add_job(executeExperimentJob, 'interval', )
+		elif data['type'] == 'foo':
+			print 'B'
+		else:
+			print 'C'
+
