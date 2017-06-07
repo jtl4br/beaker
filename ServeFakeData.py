dce4822db22cb9885be59aed394d9ffb77e53298
yoogchu <echu31@gatech.edu>
2017-06-07 18:15:03 -0400

    fake data initialization

--- ServeFakeData.py
+++ ServeFakeData.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import numpy as np
 import json
 
@@ -27,55 +26,44 @@ def connect(db, host='localhost', port=5432):
 
 
 def generateNewCustomerSignUpData(): 
-    # do your stuff
-    id = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(10))
+    _id = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(10))
     cardType = random.randint(0,2)
-    transactionType = random.randint(0,6)
-    name = fake.name()
-    region = random.randint(0,4) # SE, NE, NW, MW, SW
-    income = np.random.normal(51000,15000, 1)[0]
-    age = int(np.random.normal(37,15, 1)[0])
+    _name = fake.name()
+    _region = random.randint(0,4) # SE, NE, NW, MW, SW
+    _income = max(0, np.random.normal(51000,15000, 1)[0])
+    _age = max(0, int(np.random.normal(37,15, 1)[0]))
     previousNumCreditCards = random.randint(0,10)
 
-    print id, cardType, transactionType, name, region, income, age, previousNumCreditCards
-
-    if cardType == 0:
-    	return 0
-    elif cardType == 1:
-    	return 1
-    else:
-    	return 2
+    con, meta = connect('beaker')
+    beaker = Table('customer', meta, autoload=True)
+    i = beaker.insert()
+    i.execute(name=_name,card_type=cardType,region=_region,income=_income,age=_age,prev_num_card=previousNumCreditCards)
+    print _id, cardType, _name, _region, _income, _age, previousNumCreditCards
 
 def generateNewTransactionData():
-    id = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(10))
-    cardType = random.randint(0,2)
-    name = fake.name()
-    region = random.randint(0,4) # SE, NE, NW, MW, SW
-    income = max(0, np.random.normal(51000,15000, 1)[0])
-    age = max(0, int(np.random.normal(37,15, 1)[0]))
-    previousNumCreditCards = random.randint(0,10)
-    print id, cardType, name, region, income, age, previousNumCreditCards
-
-
-def generateNewTransactionData():
-    id = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(10))
+    _id = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(10))
     cardType = random.randint(0,2)
     transactionType = random.randint(0,6)
-    name = fake.name()
-    amount = max(0, np.random.normal(150,50, 1)[0])
-    balance = max(0, np.random.normal(2000,750, 1)[0])
-    limit = random.choice([500, 1000, 1500, 2000, 5000, 10000])
-    region = random.randint(0,4) # SE, NE, NW, MW, SW
-    income = max(0, np.random.normal(51000,15000, 1)[0])
-    age = max(0, int(np.random.normal(37,15, 1)[0]))
+    _name = fake.name()
+    _amount = max(0, np.random.normal(150,50, 1)[0])
+    _balance = max(0, np.random.normal(2000,750, 1)[0])
+    _limit = random.choice([500, 1000, 1500, 2000, 5000, 10000])
+    _region = random.randint(0,4) # SE, NE, NW, MW, SW
+    _income = max(0, np.random.normal(51000,15000, 1)[0])
+    _age = max(0, int(np.random.normal(37,15, 1)[0]))
     previousNumCreditCards = random.randint(0,10)
-    print id, cardType, transactionType, name, amount, balance, limit, region, income, age, previousNumCreditCards
+
+    con, meta = connect('beaker')
+    beaker = Table('transactions', meta, autoload=True)
+    i = beaker.insert()
+    i.execute(name=_name,card_type=cardType,transaction_type=transactionType,income=_income,region=_region,age=_age,prev_num_card=previousNumCreditCards,balance=_balance,max_limit=_limit,amount=_amount)
+    print _id, cardType, transactionType, _name, _amount, _balance, _limit, _region, _income, _age, previousNumCreditCards
 
 
 import logging
 logging.basicConfig()
 
 sched = BlockingScheduler()
-sched.add_job(generateNewCustomerSignUpData, 'interval', seconds=30)
-sched.add_job(generateNewTransactionData, 'interval', seconds=30)
+sched.add_job(generateNewCustomerSignUpData, 'interval', seconds=10)
+sched.add_job(generateNewTransactionData, 'interval', seconds=10)
 sched.start()
