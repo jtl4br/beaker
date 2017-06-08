27e732dc201239ddea58a9ee4643561e37b0bfef
yoogchu <echu31@gatech.edu>
2017-06-08 13:24:33 -0400

    view controller

--- static/partials/viewExperimentController.js
+++ static/partials/viewExperimentController.js
@@ -2,5 +2,6 @@ angular.module('myApp').controller('viewExperimentController',
 	['$scope', '$location', 'ViewAllExperimentsService',
 	function ( $scope, $location, ViewAllExperimentsService) {
 		$scope.selectedExperiment = ViewAllExperimentsService.getSelectedExperiment();
+		console.log($scope.selectedExperiment);
 
 }]);
